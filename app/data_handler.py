ab583edb8b6bba0c9ebe66b315fa79e54e091a36
Tobias Kapp <kapp.tob@gmail.com>
2024-03-13 22:50:30 +0100

    First Datahandler logic

--- app/data_handler.py
+++ app/data_handler.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.errors import ConnectionFailure
 import time
+from functools import wraps
 
 path_to_certificate="zertifikate/mongodb.pem"
 
@@ -10,14 +12,29 @@ client = MongoClient(uri,
                      tlsCertificateKeyFile=path_to_certificate,
                      server_api=ServerApi('1'))
 db = client['testDB']
-collection = db['testCol']
 
-def search_whole_table():
-    start_time = time.time()
-    result = list(collection.find())
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    return result, elapsed_time
+def time_query(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)  # Use await here if func is async
+        end_time = time.time()
+        duration = end_time - start_time
+        return result, duration
+    return wrapper
 
-if __name__ == "__main__":
-    search_whole_table()
+
+def test_connection():
+    try:
+        # The ismaster command is cheap and does not require auth.
+        client.admin.command('ismaster')
+        print("MongoDB connection is successful.")
+    except ConnectionFailure:
+        print("Server not available")
+
+@time_query
+def search_whole_table(table):
+    collection = db[table]
+    matching_documents = list(collection.find())
+    return matching_documents
+
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Query, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, Query
 from fastapi.templating import Jinja2Templates
-from data_handler import execute_query
+from fastapi.responses import JSONResponse
+
+from data_handler import search_whole_table
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -10,12 +11,11 @@ templates = Jinja2Templates(directory="templates")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/start_query")
-async def start_query(query: str = Query(...)):
-    result, query_time = execute_query(query)
+@app.get("/search_whole/{table}")
+async def search_whole(table: str):
+    result, query_time = search_whole_table(table)
     response_data = {
         'query_time': query_time,  # Add the query time to the response data
         'result': result
     }
     return JSONResponse(content=response_data)
-
