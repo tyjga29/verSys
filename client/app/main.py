8817155d71168c43995e75a0d323b36fd36402d8
Tobias Kapp <kapp.tob@gmail.com>
2024-04-04 22:53:01 +0200

    Round Robin finished

--- client/app/main.py
+++ client/app/main.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI, Request, Query
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 import requests
 import os
-import json
-
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-server = 'SmartCityReceiver1'
-port_number = int(os.environ.get("PORT_NUMBER"))
+servers = [
+    "SmartCityReceiver1",
+    "SmartCityReceiver2",
+    #"SmartCityReceiver3"
+]
+#ports= [
+#    '8080',
+#    "8081"
+#]
+port = int(os.environ.get("PORT_NUMBER"))
+
+current_server_index = 0
+def get_next_server():
+    global current_server_index
+    server = servers[current_server_index]
+    current_server_index = (current_server_index + 1) % len(servers)
+    return server
+
+#current_port_index = 0
+#def get_next_port():
+#    global current_port_index
+#    port = ports[current_port_index]
+#    current_port_index = (current_port_index + 1) % len(ports)
+#    return port
 
 @app.get("/")
 async def index(request: Request):
@@ -19,17 +37,21 @@ async def index(request: Request):
 
 @app.get("/search_whole")
 async def search_whole():
-    url = f"http://localhost:{port_number}/{server}/getData"
+    server = get_next_server()
+    #port = get_next_port()
+    url = f"http://localhost:{port}/{server}/getData"
+    print(f"Sending Get Request to {url}")
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            print("Response received successfully:")
+            print(f"Response received successfully from server {server}:")
             print("Response ", response.text)
             parsed_data = response.json()
         else:
             print(f"Failed to retrieve data. Status code: {response.status_code}")
-        print("Parsed ", parsed_data)
-        #data = jsonable_encoder(response)
+            parsed_data = {"error": f"Failed to retrieve data from {server}. Status code: {response.status_code}"}
+        print("Retrieved Data", parsed_data)
         return parsed_data
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
+        return {"error": f"An error occurred while fetching data from {server}: {e}"}
---
+++ controller2/main.py
@@ -0,0 +1,81 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from datetime import datetime
+import json
+import time
+import os
+
+from data_handler import search_whole_table
+
+server_number = 2
+
+if server_number==1:
+    address = 'SmartCityReceiver1'
+elif server_number==2:
+    address = 'SmartCityReceiver2'
+elif server_number==3:
+    address = 'SmartCityReceiver3'
+
+port_number = 8081
+
+table = "air_quality"
+
+# Caching variables
+cache = {}
+cache_expiry = 60000  # 60 seconds = 1 minute
+
+class SmartCityReceiver(BaseHTTPRequestHandler):
+    server_version = f"{address}/0.1"
+
+    def do_GET(self):
+        # Check if the request path matches '/SmartCityReceiver1'
+        if self.path == f'/{address}/getData':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            data = fetch_data(table)
+        
+            print(f"The data is {data}.")
+            print(f"The data is from {cache[table]['timestamp_db']}.")
+            print(f"Data newly retrieved at {cache[table]['timestamp_access']}.")
+            print()
+            json_data = json.dumps(data)
+            json_bytes = json_data.encode('utf-8')
+            self.wfile.write(json_data.encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+            message = "Not Found"
+            self.wfile.write(bytes(message, "utf8"))
+
+def run(server_class=HTTPServer, handler_class=SmartCityReceiver, port=port_number):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    
+    print(f'Starting {handler_class.server_version} server on port {port}...')
+    httpd.serve_forever()
+
+def fetch_data(table):
+    if table in cache and time.time() - cache[table]['timestamp_db'] < cache_expiry:
+        print("Cached data.")
+        print(f"{cache[table]['data']}")
+        return cache[table]['data']
+    else:
+        print("Data has to be newly retrieved.")
+        # Make HTTP request to fetch data
+        result = search_whole_table(table)
+        # Update cache with fetched data and timestamp
+        timestamp = 0
+        for entry in result:
+            if 'timestamp' in entry:
+                timestamp = entry['timestamp']
+                entry["timestamp"] = datetime.fromtimestamp(entry["timestamp"]).strftime('%Y-%m-%d %H:%M:%S')
+        current_time = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        cache[table] = {'data': {'result': result}, 'timestamp_db': timestamp, 'timestamp_access': current_time}
+        return {'result': result}
+
+if __name__ == '__main__':
+    run()
+
